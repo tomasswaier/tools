f88f5b8df770fd9ef2d33102263fabef6efd50e7
tomasswaier <tomasswaier@gmail.com>
2024-07-05 21:02:40 +0200

    readability fixes

--- folder_cleaner/main.py
+++ folder_cleaner/main.py
@@ -1,27 +1,19 @@
 import os
+import sys
+
+#Here are specified directories and what filetypes they expect
+filetypes = {
+    "images": ['png', 'jpg', 'jpeg'],
+    #adding directories here for safety
+    "directories": [""],
+    "documents": ['txt', 'doc', 'pdf']
+}
 
 
-#checks for dirs and files with similar names
-def dir_check(contents, filetypes):
-    #function checks if there are files inside the directories that have the same name
-    def check_insides(name):
-        subdir = os.listdir(name)
-        for file in contents:
-            if file in subdir:
-                print('cant sort:' + file)
-                contents.remove(file)
-
-    #checks if directory exists and if yest then
+def main(contents: list[str], filetypes: dict[str, list[str]]):
     for filetype in filetypes:
-        if filetype in contents:
-            check_insides(filetype)
-        else:
+        if filetype not in contents:
             os.mkdir(filetype)
-
-    return contents
-
-
-def move(contents, filetypes):
 
     for file_name in contents:
         split_file = file_name.rsplit(".", 1)
@@ -29,7 +21,12 @@ def move(contents, filetypes):
             #since python doesnt have a normal way of getting key from value I iterate over dictionary and
             for dir, type in filetypes.items():
                 if split_file[1] in type:
-                    os.rename(file_name, dir + "/" + str(file_name))
+                    dest = dir + "/" + str(file_name)
+                    if os.path.exists(dest):
+                        sys.stderr.write("file already exists: " + str(dest) +
+                                         "\n")
+                        break
+                    os.rename(file_name, dest)
                     break
         #if its a directory or something like .tar.bz2 or tar.gz it will move them here
         else:
@@ -37,17 +34,4 @@ def move(contents, filetypes):
                 os.rename(file_name, 'directories/' + str(file_name))
 
 
-def main(contents, filetypes):
-    #creates list of images to move
-    contents = dir_check(contents, filetypes)
-    #moves images
-    move(contents, filetypes)
-
-
-filetypes = {
-    "images": ['png', 'jpg', 'jpeg'],
-    #adding directories here for safety
-    "directories": [""],
-    "documents": ['txt', 'doc', 'pdf']
-}
 main(os.listdir(), filetypes)
