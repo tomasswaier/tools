f9c870fb002e36ed9b77873fbf3d4fcee98357ff
tomasswaier <tomasswaier@gmail.com>
2024-09-26 03:45:13 +0200

    semi working pain

--- paint/main.py
+++ paint/main.py
@@ -13,18 +13,11 @@ class Paint:
         # Game loop
         self.create_interface()
 
-    def create_interface(self):
-        font = pygame.font.SysFont(None, 24)
+    def width_picker(self):
 
-        self.surface = pygame.Surface(
-            (window_width * 260 / 300, window_height * 170 / 220)
-        )
-        self.screen.fill(BACKGROUND_PURPLE)
-
-        # dimentions for the width picker thingies
         pen_width_picker_dimentions = [
-            window_width * 36 / 300,
-            window_height * 80 / 220,
+            70,
+            160,
         ]
         self.pen_width_picker_border = pygame.Surface(
             (pen_width_picker_dimentions[0], pen_width_picker_dimentions[1])
@@ -34,31 +27,86 @@ class Paint:
         )
         self.pen_width_picker_inner.fill(BACKGROUND_PURPLE_DARK)
 
-        self.screen.blit(self.pen_width_picker_border, (4, window_height * 100 / 220))
-        self.screen.blit(self.pen_width_picker_inner, (5, window_height * 101 / 220))
+        self.screen.blit(self.pen_width_picker_border, (4, 210))
+        self.screen.blit(self.pen_width_picker_inner, (5, 211))
         pen_width_picker_cell_dimentions = [
             pen_width_picker_dimentions[0] / 2,
             pen_width_picker_dimentions[1] / 4,
         ]
+        self.width_picker_cells = []
+        # todo : finish adding shit
+        for i in range(4):
+            pen_width_picker_cell_1 = pygame.Surface(
+                (
+                    pen_width_picker_cell_dimentions[0],
+                    pen_width_picker_cell_dimentions[1],
+                )
+            )
+            self.width_picker_cells.append(
+                pen_width_picker_cell_1.get_rect(
+                    topleft=(5, 220 + pen_width_picker_cell_dimentions[1] * i)
+                )
+            )
 
-        self.pen_width_picker_cell_1 = pygame.Surface(
-            (pen_width_picker_cell_dimentions[0], pen_width_picker_cell_dimentions[1])
-        )
-        self.pen_width_picker_rect_1 = self.pen_width_picker_cell_1.get_rect(
-            topleft=(5, window_height * 101 / 220)
-        )
-        self.screen.blit(self.pen_width_picker_cell_1, (5, window_height * 101 / 220))
+            pen_width_picker_cell_2 = pygame.Surface(
+                (
+                    pen_width_picker_cell_dimentions[0],
+                    pen_width_picker_cell_dimentions[1],
+                )
+            )
+            self.width_picker_cells.append(
+                pen_width_picker_cell_2.get_rect(
+                    topleft=(
+                        5 + pen_width_picker_cell_dimentions[0],
+                        220 + pen_width_picker_cell_dimentions[1] * i,
+                    )
+                )
+            )
 
-        self.pen_width_picker_cell_2 = pygame.Surface(
-            (pen_width_picker_cell_dimentions[0], pen_width_picker_cell_dimentions[1])
+    def color_pallet(self):
+        self.collor_pallet_array = []
+        for i in range(int(len(COLOR_PALLET) / 2)):
+            color1 = pygame.Surface((20, 20))
+            color1.fill(pygame.Color(COLOR_PALLET[i]))
+            self.collor_pallet_array.append(
+                [
+                    color1.get_rect(topleft=(80 + (40 * i), window_height - 60)),
+                    COLOR_PALLET[i],
+                ]
+            )
+            self.screen.blit(color1, (80 + (40 * i), window_height - 60))
+            color2 = pygame.Surface((20, 20))
+            color2.fill(pygame.Color(COLOR_PALLET[i + 1]))
+            self.collor_pallet_array.append(
+                [
+                    color2.get_rect(topleft=(80 + (40 * i), window_height - 20)),
+                    COLOR_PALLET[i + 1],
+                ]
+            )
+            self.screen.blit(color2, (80 + (40 * i), window_height - 20))
+
+    def create_interface(self):
+
+        font = pygame.font.SysFont(None, 24)
+        self.surface = pygame.Surface(
+            (window_width * 260 / 300, window_height * 170 / 220)
         )
-        self.pen_width_picker_rect_2 = self.pen_width_picker_cell_2.get_rect(
-            topleft=(5 + pen_width_picker_cell_dimentions[0], window_height * 101 / 220)
+        self.screen.blit(self.surface, (window_width * 40 / 300, window_height * 0.06))
+
+        self.screen.fill(BACKGROUND_PURPLE)
+        self.surface_border = pygame.Surface(
+            ((window_width * 260 / 300) + 4, (window_height * 170 / 220) + 4)
         )
+        self.surface_border.fill(BORDER_BLUE)
         self.screen.blit(
-            self.pen_width_picker_cell_2,
-            (5 + pen_width_picker_cell_dimentions[0], window_height * 101 / 220),
+            self.surface_border,
+            ((window_width * 40 / 300) - 2, (window_height * 0.06) - 2),
         )
+        # add width picker
+        self.width_picker()
+        self.color_pallet()
+
+        # dimentions for the width picker thingies
 
         # penicon and its border
         self.pen_icon = pygame.image.load("images/pen.png").convert()
@@ -78,7 +126,6 @@ class Paint:
         self.screen.blit(label, (10, 5))
 
         self.surface.fill(WHITE)
-        self.screen.blit(self.surface, (window_width * 40 / 300, window_height * 0.06))
 
         pygame.display.flip()
 
@@ -95,19 +142,24 @@ class Paint:
                     break
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     if self.pen_icon_rect.collidepoint(pygame.mouse.get_pos()):
-                        self.pen_color = BORDER_BLUE
+                        self.pen_color = BLACK
                     if self.eraser_icon_rect.collidepoint(pygame.mouse.get_pos()):
                         self.pen_color = WHITE
-                    if self.pen_width_picker_rect_1.collidepoint(
-                        pygame.mouse.get_pos()
-                    ):
-                        self.pen_width = 1
+                    for i in range(8):  # None
 
+                        if self.width_picker_cells[i].collidepoint(
+                            pygame.mouse.get_pos()
+                        ):
+                            self.pen_width = (i + 1) ** 2
+                    for i in range(int(len(COLOR_PALLET))):
+
+                        if self.collor_pallet_array[i][0].collidepoint(
+                            pygame.mouse.get_pos()
+                        ):
+                            self.pen_color = pygame.Color(
+                                self.collor_pallet_array[i][1]
+                            )
                     # Check for collision with the second button
-                    elif self.pen_width_picker_rect_2.collidepoint(
-                        pygame.mouse.get_pos()
-                    ):
-                        self.pen_width = 2
                 elif (
                     event.type == pygame.MOUSEMOTION
                 ):  # fixing position for meow moew reasons
@@ -148,8 +200,20 @@ class Paint:
 
 
 if __name__ == "__main__":
-    window_height = 500
-    window_width = 600
+    COLOR_PALLET = [
+        "#ffe9df",
+        "#3c3772",
+        "#2d96c5",
+        "#355af5",
+        "#4adeb5",
+        "#9985f3",
+        "#e2b267",
+        "#2f62d4",
+        "#e94cb6",
+        "#de5ce8",
+    ]
+    window_height = 950
+    window_width = 1600
     color_picker = []
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
